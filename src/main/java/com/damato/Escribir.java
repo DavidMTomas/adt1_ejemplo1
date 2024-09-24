daa03a7bd7b8c2511a4c55dae9a6c5cfa3a3f43e
dmtomas <dmtomas@outlook.es>
2024-09-24 19:59:49 +0200

    repaso

--- src/main/java/com/damato/Escribir.java
+++ src/main/java/com/damato/Escribir.java
@@ -8,24 +8,24 @@ import java.io.PrintWriter;
 public class Escribir {
     static String archivo = "./src/main/resources/file.txt";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        try {
-        FileWriter fw = new FileWriter(archivo,true);
+
+        try (FileWriter fw = new FileWriter(archivo, true)) {
             for (int i = 0; i < 5; i++) {
                 fw.write(Integer.toString(i));
                 System.out.println(i);
             }
-
-            
-
-    fw.close();
-            System.out.println();
-            PrintWriter pw = new PrintWriter(new FileWriter(archivo, true));
-            pw.println("\nfrase que se escribe en el archivo");
-            pw.close();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+
+
+        System.out.println();
+        try (PrintWriter pw = new PrintWriter(new FileWriter(archivo, true))) {
+            pw.println("\nfrase que se escribe en el archivo");
+            pw.close();
+        }
+
     }
 }
